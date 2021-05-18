54e4cc740e87051e9f3884fb12a5cea448bb32cb
João Marlon <marlonsouto@gmail.com>
2021-05-18 06:56:53 -0300

    principios de Liskov

--- Arquitetura e Design de Projetos Java/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
+++ Arquitetura e Design de Projetos Java/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
@@ -1,63 +1,30 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-
-import br.com.alura.rh.ValidacaoException;
 
 public class Funcionario {
 
-	private String nome;
-	private String cpf;
-	private Cargo cargo;
-	private BigDecimal salario;
+	private DadosPessoais dadosPessoais;
 	private LocalDate dataUltimoReajuste;
 
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.cargo = cargo;
-		this.salario = salario;
+	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario, LocalDate dataUltimoReajuste) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+		this.dataUltimoReajuste = dataUltimoReajuste;
 	}
 
 	public void atualizarSalario(BigDecimal aumento) {
-		this.salario = aumento;
+		this.dadosPessoais.setSalario(aumento);
 		this.dataUltimoReajuste = LocalDate.now();
 	}
 
-	public String getNome() {
-		return nome;
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
 	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public Cargo getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
-
-	public BigDecimal getSalario() {
-		return salario;
-	}
-
-	public void setSalario(BigDecimal salario) {
-		this.salario = salario;
-	}
-
 	public LocalDate getDataUltimoReajuste() {
 		return dataUltimoReajuste;
 	}
@@ -66,4 +33,7 @@ public class Funcionario {
 		this.dataUltimoReajuste = dataUltimoReajuste;
 	}
 
+	public void promover(Cargo novoCargo) {
+		this.dadosPessoais.setCargo(novoCargo);
+	}
 }
