3093edab7c5a9c2f25ce463dbb28b95f53bb1888
João Marlon <marlonsouto@gmail.com>
2021-05-19 07:24:02 -0300

    clean

--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/LojaMain.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/LojaMain.java
@@ -2,8 +2,6 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ICMS;
-import br.com.alura.loja.imposto.ISS;
-import br.com.alura.loja.imposto.TipoImposto;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
