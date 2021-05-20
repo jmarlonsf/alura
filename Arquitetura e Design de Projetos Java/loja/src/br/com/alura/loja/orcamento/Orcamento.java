a6e9f59fb9096eb0bd8b9f71f4cdd003f842d8cb
João Marlon <marlonsouto@gmail.com>
2021-05-20 20:22:14 -0300

    exemplo de transição entre estados do objeto: pattern state

--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,32 +1,20 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
-    private String situacao;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
-    }
-
-    public void aplicarDescontoExtra() {
-        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
-
-        if (situacao.equals("EM ANALISE")) {
-            valorDoDescontoExtra = new BigDecimal("0.05");
-        } else if (situacao.equals("APROVADO")) {
-            valorDoDescontoExtra = new BigDecimal("0.02");
-        }
-
-        this.valor = this.valor.subtract(valorDoDescontoExtra);
-    }
-
-    public void aprovar() {
-        this.situacao = "APROVADO";
+        this.situacao = new EmAnalise();
     }
 
     public BigDecimal getValor() {
@@ -35,5 +23,31 @@ public class Orcamento {
 
     public int getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
     }
 }
