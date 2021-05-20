771af6bd4023b0b896db100d50b546761067acff
João Marlon <marlonsouto@gmail.com>
2021-05-20 20:51:39 -0300

    command pattern

--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/TestePedido.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/TestePedido.java
@@ -1,21 +1,17 @@
 package br.com.alura.loja;
 
-import br.com.alura.loja.orcamento.Orcamento;
-import br.com.alura.loja.pedido.Pedido;
+import br.com.alura.loja.pedido.GeraPedido;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public class TestePedido {
 
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("600"), 4);
-        String cliente = "Ana da Silva";
-        LocalDateTime data = LocalDateTime.now();
+        String cliente = args[0];
+        BigDecimal valorOrcamento = new BigDecimal(args[1]);
+        int quantidadeItens = Integer.parseInt(args[2]);
 
-        Pedido pedido = new Pedido(cliente, data, orcamento);
-
-        System.out.println("Gera pedido");
-        System.out.println("Salva pedido");
+        GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+        gerador.executa();
     }
 }
