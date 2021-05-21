47d20054c1bcae259b7c93b5ac60f1569f900ece
João Marlon <marlonsouto@gmail.com>
2021-05-21 08:11:49 -0300

    Observer

--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/pedido/acao/SalvarPedidoNoBancoDeDados.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/pedido/acao/SalvarPedidoNoBancoDeDados.java
@@ -2,7 +2,7 @@ package br.com.alura.loja.pedido.acao;
 
 import br.com.alura.loja.pedido.Pedido;
 
-public class SalvarPedidoNoBancoDeDados {
+public class SalvarPedidoNoBancoDeDados implements AcaoAposGerarPedido {
 
     public void executar(Pedido pedido) {
         System.out.println("Salvando pedido no banco de dados!");
