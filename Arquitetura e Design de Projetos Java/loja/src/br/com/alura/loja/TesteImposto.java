ff4297c59e382438f71d0c30a431b0b71eee275e
João Marlon <marlonsouto@gmail.com>
2021-05-24 06:27:18 -0300

    Exemplo Decorator

--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/TesteImposto.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/TesteImposto.java
@@ -2,6 +2,7 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -12,6 +13,6 @@ public class TesteImposto {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 
-        System.out.println(calculadora.calcular(orcamento, new ICMS()));
+        System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
     }
 }
--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/imposto/Imposto.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/imposto/Imposto.java
@@ -3,7 +3,29 @@ package br.com.alura.loja.imposto;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
-public interface Imposto {
-    BigDecimal calcular(Orcamento orcamento);
+public abstract class Imposto {
+
+    private Imposto outro;
+
+    public Imposto(Imposto outro) {
+        this.outro = outro;
+    }
+
+    protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+
+    public BigDecimal calcular(Orcamento orcamento) {
+        BigDecimal vlImposto = realizarCalculo(orcamento);
+        BigDecimal vlOutroImposto = BigDecimal.ZERO;
+
+        if (Objects.nonNull(outro)) {
+            vlOutroImposto = outro.realizarCalculo(orcamento);
+        }
+
+        return vlImposto.add(vlOutroImposto);
+
+    }
+
+
 }
