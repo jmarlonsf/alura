0e07bc7fa1c178c904277af7df02a6e8dc0d499c
João Marlon <marlonsouto@gmail.com>
2021-05-24 06:53:21 -0300

    Composite com ajuste nos anteriores para executar projeto

--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -5,25 +5,23 @@ import br.com.alura.loja.orcamento.situacao.Finalizado;
 import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Orcamento {
+public class Orcamento implements Orcavel{
 
     private BigDecimal valor;
-    private int quantidadeItens;
     private SituacaoOrcamento situacao;
+    private List<Orcavel> itens;
 
-    public Orcamento(BigDecimal valor, int quantidadeItens) {
-        this.valor = valor;
-        this.quantidadeItens = quantidadeItens;
+    public Orcamento() {
+        this.valor = BigDecimal.ZERO;
+        this.itens = new ArrayList<>();
         this.situacao = new EmAnalise();
     }
 
     public BigDecimal getValor() {
         return valor;
-    }
-
-    public int getQuantidadeItens() {
-        return quantidadeItens;
     }
 
     public SituacaoOrcamento getSituacao() {
@@ -54,5 +52,14 @@ public class Orcamento {
 
     public boolean isFinalizado() {
         return situacao instanceof Finalizado;
+    }
+
+    public int getQuantidadeItens() {
+        return this.itens.size();
+    }
+
+    public void adicionarItem(Orcavel item) {
+        this.valor = valor.add(item.getValor());
+        this.itens.add(item);
     }
 }
