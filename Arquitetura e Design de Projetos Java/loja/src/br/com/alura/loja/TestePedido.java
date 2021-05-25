f3b7a913a43b952a25571ce6f8f0e4cdde080bce
João Marlon <marlonsouto@gmail.com>
2021-05-25 05:58:15 -0300

    Facade

--- Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/TestePedido.java
+++ Arquitetura e Design de Projetos Java/loja/src/br/com/alura/loja/TestePedido.java
@@ -3,6 +3,7 @@ package br.com.alura.loja;
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
 import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.LogDePedido;
 import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
@@ -17,7 +18,10 @@ public class TestePedido {
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
         GeraPedidoHandler handler = new GeraPedidoHandler(
-                Arrays.asList(new EnviarEmailPedido(), new SalvarPedidoNoBancoDeDados())
+                Arrays.asList(
+                        new EnviarEmailPedido(),
+                        new SalvarPedidoNoBancoDeDados(),
+                        new LogDePedido())
         );
         handler.execute(gerador);
     }
