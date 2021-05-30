13eb79dcaea35af9dc0b3fa126d9de90dd5900b5
João Marlon <marlonsouto@gmail.com>
2021-05-30 10:35:21 -0300

    configurando entity manager e database para testes

--- Arquitetura e Design de Projetos Java/2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ Arquitetura e Design de Projetos Java/2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,6 +1,8 @@
 package br.com.alura.leilao.dao;
 
 import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
@@ -9,13 +11,19 @@ class UsuarioDaoTest {
 
     private UsuarioDao dao;
 
-    private EntityManager em;
-
     @Test
     void testeBuscaDeUsuarioPeloUsername() {
 
+        EntityManager em = JPAUtil.getEntityManager();
         this.dao = new UsuarioDao(em);
-        Usuario usuario = dao.buscarPorUsername("Fulano");
+
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
+
+        Usuario encontrado = dao.buscarPorUsername(usuario.getNome());
+        Assert.assertNotNull(encontrado);
 
     }
 
