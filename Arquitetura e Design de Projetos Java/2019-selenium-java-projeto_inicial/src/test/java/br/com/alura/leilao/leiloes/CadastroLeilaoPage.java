bcdb6b75e2692cfa8fcaf073ebbc7b346d9e3f6e
João Marlon <marlonsouto@gmail.com>
2021-06-03 23:32:23 -0300

    teste cadastro de leilao

--- Arquitetura e Design de Projetos Java/2019-selenium-java-projeto_inicial/src/test/java/br/com/alura/leilao/leiloes/CadastroLeilaoPage.java
+++ Arquitetura e Design de Projetos Java/2019-selenium-java-projeto_inicial/src/test/java/br/com/alura/leilao/leiloes/CadastroLeilaoPage.java
@@ -1,5 +1,6 @@
 package br.com.alura.leilao.leiloes;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CadastroLeilaoPage {
@@ -16,4 +17,12 @@ public class CadastroLeilaoPage {
         this.browser.quit();
     }
 
+    public LeiloesPage cadastrarLeilao(String nome, String valorInical, String dataAbertura) {
+        this.browser.findElement(By.id("nome")).sendKeys(nome);
+        this.browser.findElement(By.id("valorInicial")).sendKeys(valorInical);
+        this.browser.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
+        this.browser.findElement(By.id("button-submit")).submit();
+
+        return new LeiloesPage(browser);
+    }
 }
--- Arquitetura e Design de Projetos Java/2019-selenium-java-projeto_inicial/src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ Arquitetura e Design de Projetos Java/2019-selenium-java-projeto_inicial/src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -1,9 +1,14 @@
 package br.com.alura.leilao.leiloes;
 
 import br.com.alura.leilao.login.LoginPage;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LeiloesTest {
 
@@ -20,6 +25,15 @@ public class LeiloesTest {
         paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
         this.paginaDeLeiloes = paginaDeLogin.efetuaLogin();
         CadastroLeilaoPage paginaDeCadastro = this.paginaDeLeiloes.carregaFormulario();
+
+        String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String nome = "Leilão do dia " + hoje;
+        String valor = "500.00";
+
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, hoje);
+
+        Assertions.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, hoje));
+
     }
 
 }
