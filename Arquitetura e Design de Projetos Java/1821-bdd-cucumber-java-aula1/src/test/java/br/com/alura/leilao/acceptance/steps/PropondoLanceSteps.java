95ee88065fb606cc5d9a7b474c6db6d68b7481c0
João Marlon <marlonsouto@gmail.com>
2021-06-13 22:54:46 -0300

    adicionando segundo cenário, com vários lances

--- Arquitetura e Design de Projetos Java/1821-bdd-cucumber-java-aula1/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ Arquitetura e Design de Projetos Java/1821-bdd-cucumber-java-aula1/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -14,18 +14,18 @@ public class PropondoLanceSteps {
 
     private Lance lance;
     private Leilao leilao;
+    private Lance lance10;
+    private Lance lance15;
 
     @Dado("um lance valido")
     public void dado_um_lance_valido() {
         Usuario usuario = new Usuario("fulano");
         lance = new Lance(usuario, BigDecimal.TEN);
-
+        leilao = new Leilao("Tablet XPTO");
     }
 
-    @Quando("propoe o lance")
+    @Quando("propoe ao leilao")
     public void quando_propoe_o_lance() {
-
-        leilao = new Leilao("Tablet XPTO");
         leilao.propoe(lance);
     }
     @Entao("o lance eh aceito")
@@ -35,5 +35,26 @@ public class PropondoLanceSteps {
         Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
     }
 
+
+    @Dado("varios lances validos")
+    public void varios_lances_validos() {
+        Usuario usuario1 = new Usuario("fulano");
+        lance10 = new Lance(usuario1, BigDecimal.TEN);
+        Usuario usuario2 = new Usuario("Beltrano");
+        lance15 = new Lance(usuario2, new BigDecimal("15.0"));
+        leilao = new Leilao("Tablet XPTO");
+
+    }
+
+    @Quando("propoe varios lances ao leilao")
+    public void propoe_varios_lances_ao_leilao() {
+        leilao.propoe(lance10);
+        leilao.propoe(lance15);
+    }
+    @Entao("os lances sao aceitos")
+    public void os_lances_sao_aceitos() {
+        Assert.assertEquals(2, leilao.getLances().size());
+        Assert.assertEquals(new BigDecimal("15.0"), leilao.getLances().get(1).getValor());
+    }
 
 }
